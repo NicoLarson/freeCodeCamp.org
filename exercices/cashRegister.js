22f38b2132ddde5f0f145a009c8d7d886af80559
Nicolas Yang <nicolas.yang.1@gmail.com>
2020-08-07 22:24:23 +0200

    add currencyUnit array

--- exercices/cashRegister.js
+++ exercices/cashRegister.js
@@ -1,9 +1,26 @@
 function checkCashRegister(price, cash, cid) {
-    var change, status
-    return change;
+    let currencyUnit = [
+        ["PENNY", 0.01],
+        ["NICKEL", 0.05],
+        ["DIME", 0.1],
+        ["QUARTER", 0.25],
+        ["ONE", 1],
+        ["FIVE", 5],
+        ["TEN", 10],
+        ["TWENTY", 20],
+        ["ONE HUNDRED", 100]]
+
+    let cashBack = {
+        status: "",
+        change: []
+    }
+
+    let rest = cash-price
+    console.log(rest)
+    return cashBack;
 }
 
-checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
 
 
 //** Projets d'algorithmes JavaScript et de structures de données: caisse enregistreuse
@@ -16,7 +33,7 @@ cid est un tableau 2D répertoriant les devises disponibles.
 //*TODO Renvoyez {status: "INSUFFICIENT_FUNDS", change: []}si la monnaie dans le tiroir est inférieure à la monnaie due ou si vous ne pouvez pas retourner la monnaie exacte.
 //*TODO Renvoyez {status: "CLOSED", change: [...]}avec la caisse comme valeur de la clé changesi elle est égale à la monnaie due.
 //*TODO Sinon, retournez {status: "OPEN", change: [...]}, avec le changement dû en pièces et billets, triés dans l'ordre le plus élevé au plus bas, comme valeur de la changeclé.
-/* 
+/*
 checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
-devrait revenir {status: "OPEN", change: [["QUARTER", 0.5]]}. 
+devrait revenir {status: "OPEN", change: [["QUARTER", 0.5]]}.
 */
